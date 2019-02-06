ccce9fea31e4ff646253e5d8bd0659fe174170f9
maximkasatkin <cmIMav0mv0>
2019-02-06 17:58:17 +0700

    dev

--- dist/js/App.js
+++ dist/js/App.js
@@ -1,6 +1,7 @@
 const App = {
   init: function() {
     this.initMenu()
+    this.initMobileMenu()
   },
 
   initMenu: function(){
@@ -16,4 +17,26 @@ const App = {
     }
     update()
   },
+
+  initMobileMenu: function(){
+    const $burger = $('.js-burger')
+    const $shader = $('.js-shader')
+    const $mobileMenu = $('.js-mobile-menu')
+    const $menu = $('.js-menu')
+
+    $burger.on('click', function(){
+      $shader.addClass('m-active')
+      $mobileMenu.addClass('m-active')
+    })
+
+    $shader.on('click', function(){
+      $shader.removeClass('m-active')
+      $mobileMenu.removeClass('m-active')
+    })
+
+    $menu.on('click', 'a', function(){
+      $shader.removeClass('m-active')
+      $mobileMenu.removeClass('m-active')
+    })
+  }
 }
